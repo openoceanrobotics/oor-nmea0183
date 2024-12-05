d9b916aaad0a1b304cf00150c41fb37fbf302efe
Matthew Cairns <git@cairns.pro>
2024-12-04 16:35:41 -0800

    refactor: Update unittests to fully test parse function using real examples

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 pub mod sentences;
 use pest::Parser;
 use pest_derive::Parser;
-use sentences::{error::ConvertNMEA0183Error, gga::Gga, mwv::Mwv, xdr::Xdr};
+use sentences::{error::ConvertNMEA0183Error, gga::Gga, hdm::Hdm, hdt::Hdt, mwv::Mwv, xdr::Xdr};
 
 #[derive(Debug)]
 pub enum Sentence {
@@ -10,6 +10,7 @@ pub enum Sentence {
     Xdr(sentences::xdr::Xdr),
     Gga(sentences::gga::Gga),
     Hdm(sentences::hdm::Hdm),
+    Hdt(sentences::hdt::Hdt),
     // Ilt(sentences::ilt::Ilt), // Propietary
 }
 
@@ -32,7 +33,8 @@ impl NmeaParser {
             "MWV" => Sentence::Mwv(Mwv::try_from(nmea)?),
             "GGA" => Sentence::Gga(Gga::try_from(nmea)?),
             "XDR" => Sentence::Xdr(Xdr::try_from(nmea)?),
-            // "ILT" => Sentence::Ilt(Ilt::try_from(nmea)?),
+            "HDM" => Sentence::Hdm(Hdm::try_from(nmea)?),
+            "HDT" => Sentence::Hdt(Hdt::try_from(nmea)?),
             _ => Sentence::Unknown,
         })
     }
@@ -49,6 +51,7 @@ impl NmeaParser {
         let mut checksum = None;
 
         for pair in parsed.into_inner() {
+            // println!("{:?}", pair);
             match pair.as_rule() {
                 Rule::talker_id => talker_id = pair.as_str().to_string(),
                 Rule::message_type => message_type = pair.as_str().to_string(),
@@ -76,91 +79,111 @@ impl NmeaParser {
 
 #[cfg(test)]
 mod tests {
+    use sentences::{
+        gga::FixQuality,
+        xdr::{TransducerType, UnitsOfMeasurement},
+    };
+
     use super::*;
 
     #[test]
     fn test_mwv() {
         let input = "$WIMWV,049,R,000.03,N,A*03";
-        let output = NmeaParser::parse(input);
-        if let Ok(Sentence::Mwv(mwv)) = output {
-            assert_eq!(mwv.talker_id, "WI");
-            assert_eq!(mwv.message_id, "MWV");
-        }
+        let output = NmeaParser::parse(input).unwrap();
+        match output {
+            Sentence::Mwv(nmea) => {
+                assert_eq!(nmea.talker_id, "WI");
+                assert_eq!(nmea.message_id, "MWV");
+            }
+            _ => panic!("Expected Mwv"),
+        };
         let input = "$WIMWV,180,T,000.11,N,A*02";
-        let output = NmeaParser::parse(input);
-        if let Ok(Sentence::Mwv(mwv)) = output {
-            assert_eq!(mwv.talker_id, "WI");
-            assert_eq!(mwv.message_id, "MWV");
+        let output = NmeaParser::parse(input).unwrap();
+        match output {
+            Sentence::Mwv(nmea) => {
+                assert_eq!(nmea.talker_id, "WI");
+                assert_eq!(nmea.message_id, "MWV");
+            }
+            _ => panic!("Expected Mwv"),
+        }
+    }
+
+    #[test]
+    fn test_xdr() {
+        let input = "$WIXDR,C,+023.1,C,TEMP,P,0.9989,B,PRESS,H,040,P,RH*25";
+        let output = NmeaParser::parse(input).unwrap();
+        match output {
+            Sentence::Xdr(nmea) => {
+                assert_eq!(nmea.talker_id, "WI");
+                assert_eq!(nmea.message_id, "XDR");
+                if let Some(TransducerType::Temperature(r)) = &nmea.readings[0] {
+                    assert_eq!(r.reading, Some(23.1));
+                    assert_eq!(r.units, Some(UnitsOfMeasurement::Celsius));
+                    assert_eq!(r.name, Some("TEMP".into()));
+                }
+                if let Some(TransducerType::Pressure(r)) = &nmea.readings[1] {
+                    assert_eq!(r.reading, Some(0.9989));
+                    assert_eq!(r.units, Some(UnitsOfMeasurement::Bar));
+                    assert_eq!(r.name, Some("PRESS".into()));
+                }
+                if let Some(TransducerType::Pressure(r)) = &nmea.readings[2] {
+                    assert_eq!(r.reading, Some(40.0));
+                    assert_eq!(r.units, Some(UnitsOfMeasurement::Percent));
+                    assert_eq!(r.name, Some("RH".into()));
+                }
+            }
+            _ => panic!("Expected Xdr"),
         }
     }
 
     #[test]
     fn test_gga() {
         let input = "$GPGGA,113342.000,5045.7837,N,00132.4127,W,1,06,1.3,-10.2,M,47.8,M,,0000*56";
-        let output = NmeaParser::parse(input);
-        println!("{:?}", output);
-        if let Ok(Sentence::Gga(mwv)) = output {
-            assert_eq!(mwv.talker_id, "GP");
-            assert_eq!(mwv.message_id, "GGA");
+        let output = NmeaParser::parse(input).unwrap();
+        match output {
+            Sentence::Gga(nmea) => {
+                assert_eq!(nmea.talker_id, "GP");
+                assert_eq!(nmea.message_id, "GGA");
+                assert_eq!(nmea.fix_time, Some(113342.000));
+                assert_eq!(nmea.latitude, Some(5045.7837));
+                assert_eq!(nmea.longitude, Some(00132.4127));
+                assert_eq!(nmea.fix_quality, Some(FixQuality::GpsFix));
+                assert_eq!(nmea.num_satellites, Some(6));
+                assert_eq!(nmea.hdop, Some(1.3));
+                assert_eq!(nmea.altitude_msl, Some(-10.2));
+                assert_eq!(nmea.geoid_separation, Some(47.8));
+                assert_eq!(nmea.differential_age, None);
+                assert_eq!(nmea.differential_gps_reference_station_id, Some(0));
+            }
+            _ => panic!("Expected Gga"),
         }
     }
 
     #[test]
+    fn test_gilt() {
+        // let input = "$PGILT,A,+00,D,+01,D,+1,TILT*35";
+        // let output = NmeaParser::parse(input).unwrap();
+        // match output {
+        //     Sentence::Gga(nmea) => {
+        //         assert_eq!(nmea.talker_id, "GP");
+        //         assert_eq!(nmea.message_id, "GGA");
+        //         assert_eq!(nmea.fix_time, Some(113342.000));
+        //         assert_eq!(nmea.latitude, Some(5045.7837));
+        //         assert_eq!(nmea.longitude, Some(00132.4127));
+        //         assert_eq!(nmea.fix_quality, Some(FixQuality::GpsFix));
+        //         assert_eq!(nmea.num_satellites, Some(6));
+        //         assert_eq!(nmea.hdop, Some(1.3));
+        //         assert_eq!(nmea.altitude_msl, Some(-10.2));
+        //         assert_eq!(nmea.geoid_separation, Some(47.8));
+        //         assert_eq!(nmea.differential_age, None);
+        //         assert_eq!(nmea.differential_gps_reference_station_id, Some(0));
+        //     }
+        //     _ => panic!("Expected Gga"),
+        // }
+    }
+
+    #[test]
     fn test_parse_nmea_0183() {
-        let input = "$WIMWV,049,R,000.03,N,A*03";
-        let nmea = NmeaParser::to_nmea(input);
-        assert_eq!(nmea.talker_id, "WI");
-        assert_eq!(nmea.message_id, "MWV");
-        assert_eq!(nmea.fields[0], "049");
-        assert_eq!(nmea.fields[1], "R");
-        assert_eq!(nmea.fields[2], "000.03");
-        assert_eq!(nmea.fields[3], "N");
-        assert_eq!(nmea.fields[4], "A");
-
-        let input = "$WIMWV,180,T,000.11,N,A*02";
-        let nmea = NmeaParser::to_nmea(input);
-        assert_eq!(nmea.talker_id, "WI");
-        assert_eq!(nmea.message_id, "MWV");
-        assert_eq!(nmea.fields[0], "180");
-        assert_eq!(nmea.fields[1], "T");
-        assert_eq!(nmea.fields[2], "000.11");
-        assert_eq!(nmea.fields[3], "N");
-        assert_eq!(nmea.fields[4], "A");
-
-        let input = "$WIXDR,C,+023.1,C,TEMP,P,0.9989,B,PRESS,H,040,P,RH*25";
-        let nmea = NmeaParser::to_nmea(input);
-        assert_eq!(nmea.talker_id, "WI");
-        assert_eq!(nmea.message_id, "XDR");
-        assert_eq!(nmea.fields[0], "C");
-        assert_eq!(nmea.fields[1], "+023.1");
-        assert_eq!(nmea.fields[2], "C");
-        assert_eq!(nmea.fields[3], "TEMP");
-        assert_eq!(nmea.fields[4], "P");
-        assert_eq!(nmea.fields[5], "0.9989");
-        assert_eq!(nmea.fields[6], "B");
-        assert_eq!(nmea.fields[7], "PRESS");
-        assert_eq!(nmea.fields[8], "H");
-        assert_eq!(nmea.fields[9], "040");
-        assert_eq!(nmea.fields[10], "P");
-        assert_eq!(nmea.fields[11], "RH");
-
-        let input = "$GPGGA,113342.000,5045.7837,N,00132.4127,W,1,06,1.3,-10.2,M,47.8,M,,0000*56";
-        let nmea = NmeaParser::to_nmea(input);
-        assert_eq!(nmea.talker_id, "GP");
-        assert_eq!(nmea.message_id, "GGA");
-        assert_eq!(nmea.fields[0], "113342.000");
-        assert_eq!(nmea.fields[1], "5045.7837");
-        assert_eq!(nmea.fields[2], "N");
-        assert_eq!(nmea.fields[3], "00132.4127");
-        assert_eq!(nmea.fields[4], "W");
-        assert_eq!(nmea.fields[5], "1");
-        assert_eq!(nmea.fields[6], "06");
-        assert_eq!(nmea.fields[7], "1.3");
-        assert_eq!(nmea.fields[8], "-10.2");
-        assert_eq!(nmea.fields[9], "M");
-        assert_eq!(nmea.fields[10], "47.8");
-        assert_eq!(nmea.fields[11], "M");
-
         let input = "$PGILT,A,+00,D,+01,D,+1,TILT*35";
         let nmea = NmeaParser::to_nmea(input);
         assert_eq!(nmea.talker_id, "PG");
